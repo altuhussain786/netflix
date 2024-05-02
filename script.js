fb0de6dbac5e4fc0af170d52493a05a4220286f4
altaf <altuhussain786@gmail.com>
2024-05-02 11:31:11 +0530

    added script

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.getElementById("questionanswer").style.display = "block";
-document.getElementById("answer").style.visibility = "visible";
-
-function myFunction() {
-    var x = document.getElementById("#answer");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+(function() {
+	
+  document.getElementById("questionanswer").style.display = "block";
+  document.getElementById("answer").style.visibility = "visible";
+  
+  var elements = document.getElementsByClassName("questionbutton");
+  
+  function myFunction(event) {
+      var x = event.target.parentNode.parentNode.nextElementSibling;
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
     }
+    
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('click', myFunction, false);
   }
+    
+    })();
